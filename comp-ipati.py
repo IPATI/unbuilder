a7e74cc900d87bc014f3f3c28d00cf14d28ee553
Daniel Muller <danielnm@uol.com.br>
2016-10-08 10:23:35 -0300

    English translate.

--- comp-ipati.py
+++ comp-ipati.py
@@ -1,6 +1,6 @@
 #coding: utf-8
 #
-# IPATI - International Instrumental Transcommunication Research Institute - Brazil  
+# IPATI - Instrumental Transcommunication Research Institute - Brazil  
 #
 # IPATI UNBUILDER - system to shuffle voice segments to apply in transcontacts
 #
@@ -16,6 +16,8 @@
 #
 # sudo pip install pydub
 #
+# 
+# ATTENTION: calibrated to Brazilian Portuguese speech
 
 #from scikits.audiolab import *
 from pydub import AudioSegment
@@ -28,27 +30,27 @@ print '*************  IPATI UNBUILDER - COMPOSITION *******************'
 try:
 	dir_audio=sys.argv[1]
 except:
-	print "Você deve escrever o nome da pasta com os segmentos, assim: python comp-ipaty.py nome_da_pasta"
+	print "Use: python comp-ipaty.py folder"
 	quit()
 if not(os.path.exists(dir_audio)):
-	print "Não encontrei a pasta <<%s>> - verifique se o nome está correto." % dir_audio
+	print "Folder <<%s>> not found - verify if name is correct." % dir_audio
 	quit()
-arqs_dir_audio = dir_audio + '/*.wav'
-arqs=glob.glob(arqs_dir_audio)     # leitura do diretório com segmentos
-#print arqs
-tam = len(arqs)
-print "Temos %d arquivos!" % tam
-total = AudioSegment.from_wav(arqs[tam/2]) # inicializamos lendo um arquivo do meio
-lista=[tam/2]
-# como teste, estou gerando um áudio 3 vezes maior que o grupo de arquivos
-for i in range(tam*3):
-	s = random.randint(0,tam-1)
-	lista.append(s)
-	voz = AudioSegment.from_wav(arqs[s])
-	total=total+voz
+files_dir_audio = dir_audio + '/*.wav'
+files=glob.glob(files_dir_audio)     # read folder with segments files
+#print files
+length = len(files)
+print "We have %d files!" % length
+total = AudioSegment.from_wav(files[length/2]) # we start a mid file in folder
+list=[length/2]
+# to test, we generate 2 times larger than original file group
+for i in range(length*3):
+	s = random.randint(0,length-1)
+	list.append(s)
+	voice = AudioSegment.from_wav(files[s])
+	total=total+voice
 
-nomearq = dir_audio + "_composicao.wav"
+nomearq = dir_audio + "_composition.wav"
 total.export(nomearq,format="wav")
-print "Ordem de sorteio dos arquivos: ",lista
+print "Files sort order: ",list
 
 ############################ END OF FILE ############################
--- seg-ipati.py
+++ seg-ipati.py
@@ -1,6 +1,6 @@
 #coding: utf-8
 #
-# IPATI - International Instrumental Transcommunication Research Institute - Brazil  
+# IPATI - Instrumental Transcommunication Research Institute - Brazil  
 #
 # IPATI UNBUILDER - system to shuffle voice segments to apply in transcontacts
 #
@@ -25,6 +25,7 @@
 # sudo pip install --upgrade matplotlib
 # sudo pip install --upgrade pywavelets
 # 
+# ATTENTION: calibrated to Brazilian Portuguese speech
 
 from scikits.audiolab import *
 from pylab import *
@@ -35,78 +36,78 @@ import os
 
 print '*************  IPATI UNBUILDER - SEGMENTATION *******************'
 try:
-	arq_audio = sys.argv[1]
+	audio_file = sys.argv[1]
 except:
-	print "Você deve escrever o nome do arquivo de áudio, assim: python seg-ipaty.py nome_do_audio.wav"
+	print "Use: python seg-ipaty.py audio.wav"
 	quit()
-if not(os.path.exists(arq_audio)):
-	print "Não encontrei o arquivo <<%s>> - verifique se o nome está correto." % arq_audio
+if not(os.path.exists(audio_file)):
+	print "File <<%s>> not found - verify if name is correct." % audio_file
 	quit()
-nomearq=arq_audio   #('depoimento_marlene1.wav')     # leitura do arquivo
-diretorio=arq_audio[0:-4] + '_segs'
-os.mkdir(diretorio)
-voz,Fs,bits=wavread(nomearq)  
+namefile=audio_file   #('depoimento_marlene1.wav')     # read file
+directory=audio_file[0:-4] + '_segs'
+os.mkdir(directory)
+voice,Fs,bits=wavread(namefile)  
 print "freq:",Fs
 print "bits:",bits
-voz = voz / max(abs(voz))             # voz normalizada: range -1 to +1
-tam=len(voz)
-print "tamanho total:",tam
-nivel=2
-filtro='db4'  # daubechies com 4 coeficientes permite uma melhor distribuicao de coeficientes
-cca=voz
-for i in range(0,nivel):
-	cca,ccd=pywt.dwt(cca,filtro)  # wavelet discreta: ca - passa baixa e cd - passa alta 
-tam1=len(cca)
-tam2=len(ccd)
-print "tamanho vetor cca:",tam1
+voice = voice / max(abs(voice))             # normalize voice: range -1 to +1
+tam=len(voice)
+print "total length:",tam
+level=2
+filter='db4'  # daubechies 4 coefficiente allow best distribution
+cca=voice
+for i in range(0,level):
+	cca,ccd=pywt.dwt(cca,filter)  # wavelet discreta: ca - low pass and cd - high pass 
+length1=len(cca)
+length2=len(ccd)
+print "vector cca length:",length1
 
-#&&&&&&&&&&&&&&&&&&&&&&  definição de limites externos na onda
-superior=max(ccd) # usamos os coeficientes de detalhe
+#&&&&&&&&&&&&&&&&&&&&&&  external limits wave definition
+superior=max(ccd) # we use details coefficients
 inferior=max(abs(ccd))
 if superior >= inferior:
-    limite_fora=inferior
+    limit_out=inferior
 else:
-    limite_fora=superior
-limite_dentro=limite_fora * 0.1   # limite interno é 10% do externo
+    limit_out=superior
+limit_in=limit_out * 0.1   # internal limit is external 10%
 
 #@@@@@@@@@@@@@@@@@@@@@ 
-n=[0]*tam1 
-cortes=[0]*tam1
-pcortereal=[0]*tam
+n=[0]*length1 
+cuts=[0]*length1
+realcutpoint=[0]*tam
 cont=0
-abriu = False
-inicio = fim = 0
+open = False
+start = end = 0
 for i in range(2,len(cca)):
     w=cca[i]
-    if (w < limite_fora and w > -limite_fora and (w > limite_dentro or w < -limite_dentro)):
-        n[i]=w	# nova onda, sem valores extremos
+    if (w < limit_out and w > -limit_out and (w > limit_in or w < -limit_in)):
+        n[i]=w	# new wave, no extremes values
     else:
 		if (n[i-2]>0 and n[i-1]==0) or (n[i-2]==0 and n[i-1]==0): 
-			cortes[i]=1		# verifica pontos zerados na wavelet
-		if cortes[i-1] == 1: 
+			cuts[i]=1		# verify wavelet zero points 
+		if cuts[i-1] == 1: 
 			cont+=1
 		else:
 			cont == 0
 			
-		if cont >= 350:	# contagem de pontos zerados =>VALIDAR<=
-			pcortereal[i*nivel*2]=0.5	# desloca o ponto de corte para onda original
+		if cont >= 350:	# zero points count  =>TO DO VALIDATE<=
+			realcutpoint[i*level*2]=0.5	# shifts cut point to original wave
 			cont = 0
-			#print "ponto de corte: ",i*nivel*2
-			if not(abriu):
-				inicio = i*nivel*2
-				abriu = True
+			#print "cut point: ",i*level*2
+			if not(open):
+				start = i*level*2
+				open = True
 			else:
-				fim = i*nivel*2
-				if (fim-inicio)>3900:	# numero de pontos mínimo que se percebeu a vocalização
-					arq="%s/seg%d-%d.wav" % (diretorio,inicio,fim)
-					wavwrite(voz[inicio:fim],arq,Fs)
-				inicio = i*nivel*2 + 1
+				end = i*level*2
+				if (end-start)>3900:	# number of minimal point to perceive vocalization
+					file="%s/seg%d-%d.wav" % (directory,start,end)
+					wavwrite(voice[start:end],file,Fs)
+				start = i*level*2 + 1
 			
-#%%%%%%%%%%% mostra gráfico
+#%%%%%%%%%%% show graph
 '''
 figure
-plot(voz,'y')
-plot(pcortereal,'r+')
+plot(voice,'y')
+plot(realcutpoint,'r+')
 show()
 '''
 ############################ END OF FILE ############################
